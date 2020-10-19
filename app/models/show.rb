de677bc9afda2afff3be9c3f477bd0dddc97a395
Kelin Field <kfield111@hotmail.com>
2020-10-19 01:13:31 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,7 +6,7 @@ class Show < ActiveRecord::Base
   def actors_list
     array = []
     self.actors.each do |temp|
-      "#{temp.first_name} + #{temp.last_name}"
+      array << "#{temp.first_name} + #{temp.last_name}"
     end
   end
 
